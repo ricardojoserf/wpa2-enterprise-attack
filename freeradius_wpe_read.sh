f7003ad811e8fc86a043344bd5d047ccc0c63cf8
Ricardo Ruiz <ricardojruiz93@gmail.com>
2019-08-08 12:48:03 +0200

    Update freeradius_wpe_read.sh

--- freeradius_wpe_read.sh
+++ freeradius_wpe_read.sh
@@ -1,2 +1,3 @@
 #!/sh
-sudo tail /usr/local/var/log/radius/freeradius-server-wpe.log
+#sudo tail /usr/local/var/log/radius/freeradius-server-wpe.log
+cat  /usr/local/var/log/radius/freeradius-server-wpe.log  | grep -A 2 username | grep -B 1 pass | grep -v "-" | sed -ze 's/\n\tpassword//g' | sed -e 's/username://g' | sort -u | sed -e 's/\t//g'
